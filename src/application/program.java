678c81474f332cd0501a22b75124c3318a9d5173
Marcos Mecca <bmpmarcos@gmail.com>
2020-02-16 20:32:44 -0300

    First class: Position

--- src/application/program.java
+++ src/application/program.java
@@ -2,12 +2,15 @@ package application;
 
 import java.util.Scanner;
 
+import boardgame.position;
+
 public class program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello World");
+		position pos = new position(3, 5);
+		System.out.println(pos);
 		
 		sc.close();
 
