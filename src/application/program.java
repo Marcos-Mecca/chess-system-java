b7c1c6bfd1aefcebf605747134a7d796f095315b
Marcos Mecca <bmpmarcos@gmail.com>
2020-02-16 19:07:01 -0300

    Project Created

---
+++ src/application/program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Hello World");
+		
+		sc.close();
+
+	}
+
+}
